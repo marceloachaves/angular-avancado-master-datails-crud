6c0c5435bee4d1be38ffea12f88a86d98d606a88
marceloac54 <marceloac543@gmail.com>
2019-02-13 21:59:43 -0200

    entrylistcomponent

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 import { Category } from './pages/categories/shared/category.model';
+import { Entry } from './pages/entries/shared/entry.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
@@ -9,9 +10,22 @@ export class InMemoryDatabase implements InMemoryDbService {
             { id: 2, name: 'Saúde', description: 'Planos de saúde e remédios'},
             { id: 3, name: 'Lazer', description: 'Cinema, parques, praia, etc'},
             { id: 4, name: 'Salário', description: 'Recebimento de Salário'},
-            { id: 5, name: 'Freelas', description: 'Trabalhos como freelas'},
+            { id: 5, name: 'Freelas', description: 'Trabalhos como freelas'}
         ];
 
-        return { categories };
+        const entries: Entry[] = [
+            { id: 1, name: 'Gas de cozinha', categoryId: categories[0].id, category: categories[0],
+                paid: true, date: '14/10/2018', amount: '70,80', type: 'revenue', description: 'Qualquer descricao' } as Entry,
+            { id: 2, name: 'Suplementos', categoryId: categories[1].id, category: categories[1],
+                paid: false, date: '14/10/2018', amount: '70,80', type: 'expense', description: 'Qualquer suplementos' } as Entry,
+            { id: 3, name: 'Salário na empresa X', categoryId: categories[2].id, category: categories[2],
+                paid: true, date: '14/10/2018', amount: '70,80', type: 'revenue', description: 'Qualquer salario' } as Entry,
+            { id: 4, name: 'Video Game filha', categoryId: categories[3].id, category: categories[3],
+                paid: false, date: '14/10/2018', amount: '70,80', type: 'expense', description: 'Qualquer video game' } as Entry,
+            { id: 5, name: 'Cinema', categoryId: categories[4].id, category: categories[4],
+                paid: true, date: '14/10/2018', amount: '70,80', type: 'expense', description: 'Qualquer cinema' } as Entry
+        ];
+
+        return { categories, entries };
     }
 }
---
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Entry } from '../shared/entry.model';
+import { EntryService } from '../shared/entry.service';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css']
+})
+export class EntryListComponent implements OnInit {
+
+  entries: Entry[] = [];
+
+  constructor(private entryService: EntryService) { }
+
+  ngOnInit() {
+    this.entryService.getAll().subscribe(
+      entries => this.entries = entries,
+      error => alert('Erro ao carregar a lista' + JSON.stringify(error, null, 4))
+    );
+  }
+
+  deleteEntry(entry) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
+    if (mustDelete) {
+      this.entryService.delete(entry.id).subscribe(
+        () => this.entries = this.entries.filter(element => element !== entry),
+        () => alert('Erro ao tentar excluir')
+      );
+    }
+  }
+}
